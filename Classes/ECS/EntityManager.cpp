1126d1d1086936dfe4b4f0a1c2009f81da760801
lmartinignacio <lmartinignacio@gmail.com>
2019-10-25 01:53:14 -0300

    Adding ECS Engine

--- Classes/ECS/EntityManager.cpp
+++ Classes/ECS/EntityManager.cpp
@@ -1,12 +1,20 @@
 #include "EntityManager.h"
 
-
-
 EntityManager::EntityManager()
 {
+	m_id = 0;
 }
-
 
 EntityManager::~EntityManager()
 {
+}
+
+unsigned int EntityManager::CreateEntity()
+{
+	m_id = 0;
+
+	while (m_entities.find(m_id) != m_entities.end())
+		++m_id;
+
+	return m_id;
 }
--- Classes/ECS/EntityManager.h
+++ Classes/ECS/EntityManager.h
@@ -1,8 +1,20 @@
 #pragma once
+
+#include <map>
+
+using ContainerEntity = std::map<unsigned int, unsigned int>;
+//The second unsigned int will be user later to find faster the network id
+
 class EntityManager
 {
 public:
 	EntityManager();
 	~EntityManager();
+
+	unsigned int CreateEntity();
+
+private:
+	unsigned int m_id;
+	ContainerEntity m_entities;
 };
 
